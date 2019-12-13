3c8e24cf84cf26c0ed7509e961956a8d1b182d21
Trea Hauet <trea@treahauet.com>
2019-12-12 23:47:29 -0700

    add api docs collection to production config

--- config.production.php
+++ config.production.php
@@ -3,4 +3,22 @@
 return [
     'baseUrl' => 'https://docs.zippopotam.us',
     'production' => true,
+    'collections' => [
+        'docs' => [
+            'extends' => '_layouts/documentation',
+            'items' => function() {
+
+                $generator = app(\App\Data\GenerateApiDocs::class);
+                $docs = require("api_docs.php");
+
+                return collect($docs['versions'])->map(function($v) use ($generator) {
+                    return [
+                        'title' => $v['title'],
+                        'filename' => $v['path'],
+                        'content' => $generator->generate($v),
+                    ];
+                });
+            },
+        ]
+    ],
 ];
